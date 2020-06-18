f9644bcf541c3b4cbcc2dec67f69e5b26281c04d
aaron-schuyler <asky28@gmail.com>
2020-06-18 19:22:17 -0400

    Done.

--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
-  def up
-  end
-  
-  def down
+  def change
+    create_table :users do |t|
+      t.text :username
+      t.string :password_digest
+    end
   end
 end
